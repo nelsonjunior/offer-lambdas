070e4a3a4ef96a0b74716723b354a347a6390ca7
Nelson <nrsjnet@gmail.com>
2022-09-30 20:02:44 -0300

    Ajuste lambda

--- lambda-upload-get-signed-url/index.ts
+++ lambda-upload-get-signed-url/index.ts
@@ -1,20 +1,22 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResultV2} from "aws-lambda";
-import {S3Customizations} from "aws-sdk/lib/services/s3";
 import {uuidv4} from "uuid";
 import {APIGatewayProxyStructuredResultV2} from "aws-lambda/trigger/api-gateway-proxy";
+import {S3} from "aws-sdk";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
 
-    const s3Client = new S3Customizations({
-        region: 'us-east-1'
-    });
+    console.info(`request from: ${event.path} params: ${event.pathParameters}`);
 
-    const result = await getSignedUploadURL(s3Client);
+    const result = await getSignedUploadURL();
+
     console.log('Result: ', result);
+
     return result;
 }
 
-async function getSignedUploadURL(s3Client: S3Customizations): Promise<APIGatewayProxyStructuredResultV2> {
+async function getSignedUploadURL(): Promise<APIGatewayProxyStructuredResultV2> {
+
+    const s3 = new S3({region: 'us-east-1'});
     const actionId = uuidv4();
     const expires = 30 * 60; // 30 minutes in seconds
     const fileExtension = '.jpg';
@@ -28,7 +30,7 @@ async function getSignedUploadURL(s3Client: S3Customizations): Promise<APIGatewa
 
     console.log('getUploadURL: ', s3Params);
 
-    const signedURL = s3Client.getSignedUrlPromise('putObject', s3Params);
+    const signedURL = s3.getSignedUrlPromise('putObject', s3Params);
 
     return signedURL.then((url: string) => {
         return new Promise((resolve, reject) => {
